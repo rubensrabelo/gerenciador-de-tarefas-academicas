5b5fd747c623f14ae12884fcd780af7f06432042
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-10-03 14:08:11 -0300

    chore(project): create the project structure of the task microservice

---
+++ backend/task/src/test/java/io/github/rubensrabelo/ms/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rubensrabelo.ms.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
