89d73e64578cfdcb7292a82f968ce998df1544de
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-10-03 14:21:14 -0300

    feat(ms-task): add domain

---
+++ backend/task/src/main/java/io/github/rubensrabelo/ms/task/domain/Task.java
@@ -0,0 +1,97 @@
+package io.github.rubensrabelo.ms.task.domain;
+
+import io.github.rubensrabelo.ms.task.domain.enums.TaskStatus;
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_task")
+public class Task implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(length = 200)
+    private String description;
+    private LocalDate dueDate;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    public Task() {
+    }
+
+    public Task(String title, String description, LocalDate dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.status = TaskStatus.PENDING;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ backend/task/src/main/java/io/github/rubensrabelo/ms/task/domain/enums/TaskStatus.java
@@ -0,0 +1,9 @@
+package io.github.rubensrabelo.ms.task.domain.enums;
+
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED,
+    OVERDUE;
+}
