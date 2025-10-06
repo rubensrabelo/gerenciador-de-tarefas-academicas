f9fd9531edc85f66a0e240ee69639c17c04230b3
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-10-06 10:29:53 -0300

    config(api-gateway): add cors

---
+++ backend/gateway/src/main/java/io/github/rubensrabelo/gateway/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package io.github.rubensrabelo.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        cors.setAllowedHeaders(List.of("*"));
+        cors.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cors);
+
+        return new CorsWebFilter(source);
+    }
+}
