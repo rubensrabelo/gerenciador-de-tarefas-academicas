7a20b5f17b6dd6e4c9bbb6b18b4e6f1e09f741f4
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-10-14 10:27:55 -0300

    chore(backend-task-overdue-checker): start project with dependencies

---
+++ backend/task-overdue-checker/src/main/java/io/github/rubensrabelo/ms/task_overdue_checker/TaskOverdueCheckerApplication.java
@@ -0,0 +1,13 @@
+package io.github.rubensrabelo.ms.task_overdue_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOverdueCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskOverdueCheckerApplication.class, args);
+	}
+
+}
---
+++ backend/task-overdue-checker/src/test/java/io/github/rubensrabelo/ms/task_overdue_checker/TaskOverdueCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rubensrabelo.ms.task_overdue_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOverdueCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
